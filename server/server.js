faf659b776a958fe8f2b769be2482c5d176fb440
Madhusmita Sen <madhusmitasen27@gmail.com>
2025-09-25 13:12:07 +0530

    DBFIXED

--- server/server.js
+++ server/server.js
@@ -41,6 +41,6 @@ app.use((err, req, res, next) => {
   res.status(500).json({ error: "Server error" });
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
 });
