1bd4b7175d0df2d8e03397fe8cc8c50e8ea3f28a
Madhusmita Sen <madhusmitasen27@gmail.com>
2025-09-26 00:08:47 +0530

    added new vercel URL

--- server/server.js
+++ server/server.js
@@ -12,7 +12,8 @@ const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
   "http://localhost:5173",
   "https://collab-suite.vercel.app",
-  "https://collab-suite-4ek412jef-madhusmita-sen-s-projects.vercel.app", // your actual deploy
+  "https://collab-suite-4ek412jef-madhusmita-sen-s-projects.vercel.app",
+  "https://collab-suite-weld.vercel.app", // your actual deploy
 ];
 
 app.use(cors({
